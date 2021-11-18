09e4b9e173f939c37ff26ea7392224c4b64fc638
priscila monteiro <pririmonteiro@me.com>
2021-11-18 13:40:28 -0800

    testing seeds

--- api/data/seeds/02_potlucks.js
+++ api/data/seeds/02_potlucks.js
@@ -6,22 +6,22 @@ exports.seed = function (knex) {
       // Inserts seed entries
       return knex('potlucks').insert([
         {
-          potluck_name: 'rowValue1',
+          potluck_name: 'Thanksgiving',
           date: '9-30-2021',
           time: '12:00:00',
           user_id: 1,
           location: 'U.S.A'
         },
         {
-          potluck_name: 'rowValue2',
-          date: '10-30-2021',
+          potluck_name: 'Xmas',
+          date: '12-25-2021',
           time: '13:00:00',
           user_id: 1,
           location: 'U.S.A'
         },
         {
-          potluck_name: 'rowValue3',
-          date: '10-30-2021',
+          potluck_name: 'Xmas Eve',
+          date: '12-24-2021',
           time: '13:00:00',
           user_id: 1,
           location: 'U.S.A'
