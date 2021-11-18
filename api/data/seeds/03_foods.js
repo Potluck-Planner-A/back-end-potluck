f5eb750891fe66eafe15beed2f2aa409293194ab
priscila monteiro <pririmonteiro@me.com>
2021-11-18 12:47:17 -0800

    foods list

--- api/data/seeds/03_foods.js
+++ api/data/seeds/03_foods.js
@@ -11,6 +11,21 @@ exports.seed = function (knex) {
         {
           food_name: 'cramberry sauce'
         },
+        {
+          food_name: 'special rice'
+        },
+        {
+          food_name: 'mashed potato'
+        },
+        {
+          food_name: 'tacos'
+        },
+        {
+          food_name: 'pizza'
+        },
+        {
+          food_name: 'guacamole'
+        },
       ]);
     });
 };
